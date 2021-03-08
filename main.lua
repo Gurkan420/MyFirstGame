ad54988962f5f42ee9717faf478b254facd645d9
August Ljungdell <august.ljungdell@skola.botkyrka.se>
2021-03-08 15:57:34 +0100

    update

--- main.lua
+++ main.lua
@@ -1,66 +1,60 @@
+
 platform = {}
 player = {}
  
 function love.load()
-
-
-
-        
-	platform.width = love.graphics.getWidth()    --bredd
-	platform.height = love.graphics.getHeight()  --höjd
+	platform.width = love.graphics.getWidth()
+	platform.height = love.graphics.getHeight()
  
-        
-	platform.x = 0                             
-	platform.y = 500      
-    
-    player.x = love.graphics.getWidth() / 2 --spelare i mitten
-	player.y = 500 
-    player.img = love.graphics.newImage('purple.png')
-
-    player.speed = 250
-    player.ground = player.y     -- landar på platformen
+	platform.x = 0
+	platform.y = platform.height / 2
  
-	player.y_velocity = 0        -- när spelaren inte hoppat           tror jag missat något, går ej att hoppa?
+	player.x = love.graphics.getWidth() / 2
+	player.y = love.graphics.getHeight() / 2
  
-	player.jump_height = -300    -- hur högt spelaren kan hoppa
-	player.gravity = -500   
+	player.speed = 200
+ 
+	player.img = love.graphics.newImage('purple.png')
+ 
+	player.ground = player.y
+ 
+	player.y_velocity = 0
+ 
+	player.jump_height = -300
+	player.gravity = -500
 end
-
+ 
 function love.update(dt)
-
-    if love.keyboard.isDown('d') then                   
-		player.x = player.x + (player.speed * dt) 
-           
-	elseif love.keyboard.isDown('a') then               
-		player.x = player.x - (player.speed * dt)    
-        
-
-	end
-
-    function love.keypressed( key )
-        player.jumpVel = -10
-        if key == ('w') and playerCanJump then
-          player.yvel = player.jumpVel
-          playerCanJump = false
-        end
-      end
-if love.keyboard.isDown('d') then
-		--funkade inte att skapa en border vid sidorna
+	if love.keyboard.isDown('d') then
 		if player.x < (love.graphics.getWidth() - player.img:getWidth()) then
 			player.x = player.x + (player.speed * dt)
 		end
 	elseif love.keyboard.isDown('a') then
-		--andra sidan
 		if player.x > 0 then 
 			player.x = player.x - (player.speed * dt)
 		end
 	end
+ 
+	if love.keyboard.isDown('space') then
+		if player.y_velocity == 0 then
+			player.y_velocity = player.jump_height
+		end
+	end
+ 
+	if player.y_velocity ~= 0 then
+		player.y = player.y + player.y_velocity * dt
+		player.y_velocity = player.y_velocity - player.gravity * dt
+	end
+ 
+	if player.y > player.ground then
+		player.y_velocity = 0
+    	player.y = player.ground
+	end
 end
  
 function love.draw()
-	love.graphics.setColor(1, 1, 1)       --vit platform
- 
-   
+	love.graphics.setColor(1, 1, 1)
 	love.graphics.rectangle('fill', platform.x, platform.y, platform.width, platform.height)
-    love.graphics.draw(player.img, player.x, player.y, 0, 1, 1, 0, 32)
+ 
+	love.graphics.draw(player.img, player.x, player.y, 0, 1, 1, 0, 32)
 end
