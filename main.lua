3bca3da355eeca74ccc70d0d841827ff7fa95e14
August Ljungdell <august.ljungdell@skola.botkyrka.se>
2021-04-20 20:07:43 +0200

    fixed many things

--- main.lua
+++ main.lua
@@ -1,73 +1,124 @@
-platform = {}
-player = {}
-player.y = 0
-player.x = 0
-player.img = love.graphics.newImage("Gubbe.png")
-platform.y = 100
-platform.x = 0
-platform.height = 0
-platform.width = 0
-
-
-
 
 
 function love.load()
-	platform.width = love.graphics.getWidth()
-	platform.height = love.graphics.getHeight()
- 
-	platform.x = 0
-	platform.y = 500
- 
-	player.x = love.graphics.getWidth() / 2
-	player.y = love.graphics.getHeight() / 2
- 
-	player.speed = 200
- 
-	player.ground = player.y
- 
-	--player.y.speed = 0
- 
-	--player.jump.height = -300
-	player.gravity = -500
+
+    ybirdGravity = 0
+    ybird = 180
+    birdX = 62
+    birdWidth = 30
+
+    worldWidth = 800
+    worldHeight = love.graphics.getHeight()
+    gameOver = false
+   
+    pipeSpaceHeight = 75
+    pipeSpaceY = love.math.random(0, worldHeight - pipeSpaceHeight)
+    pipeX = worldWidth
+    widthPipe = 250
+   
+    playerScore = 0
+    highScore = 0
+    highScore = loadHighScore()
+
+    pipeReset()
+
 end
- function player.gravity(dt)
-	 if player.y > 0 then
-		 player.y = player.y - (10 * dt)
-	 end
- end
+
+function pipeReset()
+
+    local pipeSpaceYMin = 30
+    
+    pipeSpaceY = love.math.random(pipeSpaceYMin, worldHeight - pipeSpaceHeight - pipeSpaceYMin)
+    pipeX = worldWidth
+    widthPipe = 20
+    
+    playerScore = playerScore + 1
+
+end
 
 function love.update(dt)
-	
-		if love.keyboard.isDown("d") then
-			player.x = player.x + (player.speed * dt)
-	
-	
-		elseif love.keyboard.isDown("a") then
-			player.x = player.x - (player.speed * dt)
-		--if love.keyboard.isDown("space") then
 
-		
-	--	if player.y.speed == 0 then
-		--		player.y.speed = player.jump.height
-		--	end
-		--	if player.y.speed ~= 0 then
-		--		player.y = player.y + player.y_velocity * dt
-			--	player.y_velocity = player.y_velocity - player.gravity * dt 
-			
-		--	end
-	--	end
-		end
+    ybirdGravity = ybirdGravity + (500 * dt)
+    ybird = ybird + (ybirdGravity * dt)
+    pipeX = pipeX - (400  * dt)
+
+    if (pipeX + widthPipe) < -widthPipe then pipeReset() 
+    end
+
+    if birdX < (pipeX + widthPipe) and (birdX + birdWidth) > pipeX and (ybird < pipeSpaceY) then 
+        gameOver = true
+    end
+
+    if birdX < (pipeX + widthPipe) and (birdX + birdWidth) > pipeX and (ybird > pipeSpaceY + pipeSpaceHeight) then 
+        gameOver = true
+    end
+
+    if (ybird > worldHeight) then  
+        gameOver = true 
+    end
+
+    if gameOver == true then 
+                
+    if (tonumber(highScore) < playerScore) then
+         highScore = playerScore
+         saveHighScore(highScore)
+    end
+
+      love.event.quit("restart") 
+
+    end
 end
---function love.keypressed(key)
---	if key = "space" then
 
---	end
---end
+function love.keypressed(key) 
+    
+  if key == ("space") then 
+     ybirdGravity = -200 
+  end 
+
+end
+
 function love.draw()
-	love.graphics.draw(player.img, player.x,player.y,0,2,2,0,32) -- player
 
-	love.graphics.setColor(1, 1, 1) --background
-	love.graphics.rectangle('fill', platform.x, platform.y, platform.width, platform.height) --background
+    love.graphics.setColor(1, 0, 2, 0, 9)
+    love.graphics.rectangle("fill", 0, 0, worldWidth, worldHeight)
+
+    love.graphics.setColor(.70, .90, .30)
+    love.graphics.rectangle('fill', birdX, ybird, 15, 20)
+
+    local pipeWidth = 25
+
+    love.graphics.setColor(.37, .82, .28)
+
+    love.graphics.rectangle('fill', pipeX, 0, pipeWidth, pipeSpaceY)
+
+    love.graphics.rectangle('fill', pipeX, pipeSpaceY + pipeSpaceHeight, pipeWidth, worldHeight - pipeSpaceY - pipeSpaceHeight)
+
+    local font = love.graphics.newFont(18)
+    love.graphics.setFont(font)
+
+	love.graphics.print({"SCORE: " .. playerScore}, 10, 10)
+
+	love.graphics.print({"HIGH SCORE: " .. highScore}, love.graphics.getWidth() - font:getWidth("HIGH SCORE: 999"), 10)
+
+end
+
+
+function loadHighScore() 
+
+    local highscore = love.filesystem.read("highscore.sav")
+
+    if(highscore == nil) then
+        highscore = 0
+    end
+
+    return highscore
+
+end
+
+
+function saveHighScore(newHighScore)
+
+    love.filesystem.write("highscore.sav", newHighScore)
+
 end
 
--- maintest.lua
+++ maintest.lua
@@ -1,48 +1,92 @@
+function love.load()
+    playingAreaWidth = 300
+    playingAreaHeight = 388
 
+    birdX = 62
+    birdWidth = 30
+    birdHeight = 25
 
-function love.load(dt)
-    playerX = 20
-    playerY = 525
-    square = {}
-    base = {}
-    baseY = 550
-    baseX = 0
-        world = love.physics.newWorld(0, 9.81*640, true)
-        world = love.physics.newWorld(0, 9.81*64, true )
-        square.body = love.physics.newBody(world, playerX, playerY, "dynamic")
-        square.shape = love.physics.newRectangleShape(25, 25)
-        square.fixture = love.physics.newFixture(square.body, square.shape, 1)
-        base.shape = love.physics.newRectangleShape(50, 50)
-        base.body = love.physics.newBody(world, baseY, baseX, "static")
-        base.fixture = love.physics.newFixture(base.body, base.shape, 1)
-    
+    pipeSpaceHeight = 100
+    pipeWidth = 54
+
+    function newPipeSpaceY()
+        local pipeSpaceYMin = 54
+        local pipeSpaceY = love.math.random(
+            pipeSpaceYMin,
+            playingAreaHeight - pipeSpaceHeight - pipeSpaceYMin
+        )
+        return pipeSpaceY
     end
-    
+
+    function reset()
+        birdY = 200
+        birdYSpeed = 0
+
+        pipe1X = playingAreaWidth
+        pipe1SpaceY = newPipeSpaceY()
+
+        pipe2X = playingAreaWidth + ((playingAreaWidth + pipeWidth) / 2)
+        pipe2SpaceY = newPipeSpaceY()
+
+        score = 0
+
+        upcomingPipe = 1
+    end
+
+    reset()
+end
+
+function love.update(dt)
+    -- etc.
+
+    if isBirdCollidingWithPipe(pipe1X, pipe1SpaceY)
+    or isBirdCollidingWithPipe(pipe2X, pipe2SpaceY)
+    or birdY > playingAreaHeight then
+        reset()
+    end
+
     function love.update(dt)
-        world:update(dt)
-        if love.keyboard.isDown("a") then
-            playerX = playerX - 200 * dt
+    
+        function isBirdCollidingWithPipe(pipeX, pipeSpaceY)
+            return
+            -- Left edge of bird is to the left of the right edge of pipe
+            birdX < (pipeX + pipeWidth)
+            and
+             -- Right edge of bird is to the right of the left edge of pipe
+            (birdX + birdWidth) > pipeX
+            and (
+                -- Top edge of bird is above the bottom edge of first pipe segment
+                birdY < pipeSpaceY
+                or
+                -- Bottom edge of bird is below the top edge of second pipe segment
+                (birdY + birdHeight) > (pipeSpaceY + pipeSpaceHeight)
+            )
         end
-            if love.keyboard.isDown("d") then
-                playerX = playerX + 200 * dt
-            
-            end
-            if love.keyboard.isDown("space") then
-                playerY = playerY - 100 * dt
-            
-            end
+    
+        if isBirdCollidingWithPipe(pipe1X, pipe1SpaceY)
+        or isBirdCollidingWithPipe(pipe2X, pipe2SpaceY) then
+            love.load()
         end
-    
-    
-        
-    function love.draw()
-        love.graphics.setColor(1, 0.5, 0)
-        love.graphics.rectangle("fill",playerX,playerY,25,25)	 --player
-        love.graphics.setColor(0, 0,9, 1)
-        love.graphics.rectangle("fill", baseX, baseY, 1000, 500) --base
-    
-        
-        background = love.graphics.setBackgroundColor(0, 0, .118) --background
+
+    function isBirdCollidingWithPipe(pipeX, pipeSpaceY)
+        return
+        -- Left edge of bird is to the left of the right edge of pipe
+        birdX < (pipeX + pipeWidth)
+        and
+         -- Right edge of bird is to the right of the left edge of pipe
+        (birdX + birdWidth) > pipeX
+        and (
+            -- Top edge of bird is above the bottom edge of first pipe segment
+            birdY < pipeSpaceY
+            or
+            -- Bottom edge of bird is below the top edge of second pipe segment
+            (birdY + birdHeight) > (pipeSpaceY + pipeSpaceHeight)
+        )
     end
-    
-    
+
+    if isBirdCollidingWithPipe(pipe1X, pipe1SpaceY)
+    or isBirdCollidingWithPipe(pipe2X, pipe2SpaceY) then
+        love.load()
+    end
+end
+end
